518319f1cee1b56f2d9d546df509fbc8531785d9
aakbarkhan <aakbarkhan@gmail.com>
2022-04-07 00:07:59 +0530

    Add the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/new'
+  # get 'users/index'
+  # get 'users/new'
   devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # root to: "users#index"
-  # Defines the root path route ("/")
-  # root "articles#index"
+  unauthenticated :user do
+    root to: "users#index"
+  end
+
+  authenticated :user do
+    root to: "categories#index", as: :authenticated_root
+  end
+  resources :categories do
+    resources :expenditures
+  end
+
+  devise_scope  :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
+  
 end
