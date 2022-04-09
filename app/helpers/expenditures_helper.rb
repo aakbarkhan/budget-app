a8c68a103661a15a4446b0ec6e8eed56a26315f1
aakbarkhan <aakbarkhan@gmail.com>
2022-04-09 15:48:48 +0530

    del the img

--- app/helpers/expenditures_helper.rb
+++ app/helpers/expenditures_helper.rb
@@ -1,6 +1,6 @@
 module ExpendituresHelper
   def example_img(category)
-    img_example = '/assets/user.png'
+    img_example = 'https://files.slack.com/files-pri/T02KUTEGASX-F03B7A2L2KB/user.png'
     category.icon.present? ? category.icon = img_example : img_example
   end
 end
